ab31a367a389505315f7cbbbbc8e06b0b7734ae7
Cam Taliadoros <84043785+camtaliadoros@users.noreply.github.com>
2022-02-15 22:39:10 +0000

    card styling

--- components/match-listing/MatchCard.js
+++ components/match-listing/MatchCard.js
@@ -1,6 +1,13 @@
 import { selectMatches } from '../../features/matches/matchesSlice';
 import { useSelector } from 'react-redux';
 import classes from './matchListing.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faCalendar,
+  faClock,
+  faUserGroup,
+  faLocationDot,
+} from '@fortawesome/free-solid-svg-icons';
 
 export default function MatchCard() {
   const matchData = useSelector(selectMatches);
@@ -14,71 +21,31 @@ export default function MatchCard() {
   return (
     <div className='card'>
       {onWaitlist ? <p className={classes.waitlistText}>On Waitlist</p> : null}
-      <div className={classes.matchData}>
-        <div>
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            width='17.024'
-            height='18.694'
-            viewBox='0 0 17.024 18.694'
-          >
-            <g
-              id='Icon_feather-calendar'
-              data-name='Icon feather-calendar'
-              transform='translate(1 1)'
-            >
-              <path
-                id='Path_1'
-                data-name='Path 1'
-                d='M6.169,6H17.855a1.669,1.669,0,0,1,1.669,1.669V19.355a1.669,1.669,0,0,1-1.669,1.669H6.169A1.669,1.669,0,0,1,4.5,19.355V7.669A1.669,1.669,0,0,1,6.169,6Z'
-                transform='translate(-4.5 -4.331)'
-                fill='none'
-                stroke='#adbca9'
-                stroke-linecap='round'
-                stroke-linejoin='round'
-                stroke-width='2'
-              />
-              <path
-                id='Path_2'
-                data-name='Path 2'
-                d='M24,3V6.339'
-                transform='translate(-13.149 -3)'
-                fill='none'
-                stroke='#adbca9'
-                stroke-linecap='round'
-                stroke-linejoin='round'
-                stroke-width='2'
-              />
-              <path
-                id='Path_3'
-                data-name='Path 3'
-                d='M12,3V6.339'
-                transform='translate(-7.827 -3)'
-                fill='none'
-                stroke='#adbca9'
-                stroke-linecap='round'
-                stroke-linejoin='round'
-                stroke-width='2'
-              />
-              <path
-                id='Path_4'
-                data-name='Path 4'
-                d='M4.5,15H19.524'
-                transform='translate(-4.5 -8.322)'
-                fill='none'
-                stroke='#adbca9'
-                stroke-linecap='round'
-                stroke-linejoin='round'
-                stroke-width='2'
-              />
-            </g>
-          </svg>
-
-          {matchDate}
+      <div className={classes.matchDataWrapper}>
+        <div className={classes.matchData}>
+          <div className={classes.iconContainer}>
+            <FontAwesomeIcon icon={faCalendar} className='icon' />
+          </div>
+          <p>{matchDate}</p>
         </div>
-        <div>{matchTime}</div>
-        <div>{matchGroupID}</div>
-        <div>{matchLocation}</div>
+        <div className={classes.matchData}>
+          <div className={classes.iconContainer}>
+            <FontAwesomeIcon icon={faClock} className='icon' />
+          </div>
+          <p>{matchTime}</p>
+        </div>
+        <div className={classes.matchData}>
+          <div className={classes.iconContainer}>
+            <FontAwesomeIcon icon={faUserGroup} className='icon' />
+          </div>
+          <p>{matchGroupID}</p>
+        </div>
+        <div className={classes.matchData}>
+          <div className={classes.iconContainer}>
+            <FontAwesomeIcon icon={faLocationDot} className='icon' />
+          </div>
+          <p>{matchLocation}</p>
+        </div>
       </div>
     </div>
   );
--- components/match-listing/UpcomingMatchesListing.js
+++ components/match-listing/UpcomingMatchesListing.js
@@ -1,13 +1,16 @@
 import Link from 'next/link';
 import MatchCard from './MatchCard';
 import classes from './matchListing.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleRight } from '@fortawesome/free-solid-svg-icons';
 
 export default function UpcomingMatchesListing({ display }) {
   return (
     <div className={classes.listingWrapper}>
-      <Link href='./upcoming-matches'>
-        <a className='title'>Upcoming Matches</a>
-      </Link>
+      {/* <Link href='./upcoming-matches'> */}
+      <a className='title'>Upcoming Matches</a>
+      <FontAwesomeIcon icon={faAngleRight} className='icon' />
+      {/* </Link> */}
       <MatchCard />
     </div>
   );
