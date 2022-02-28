ae4107597eaa1a106c4a9df7f3c6b54603bc4bb7
Cam Taliadoros <84043785+camtaliadoros@users.noreply.github.com>
2022-02-28 17:39:38 +0000

    get group players fn

--- features/group/groupsSlice.js
+++ features/group/groupsSlice.js
@@ -1,12 +1,19 @@
-import { createSlice } from '@reduxjs/toolkit';
-import { createAsyncThunk } from '@reduxjs/toolkit';
-import { doc, setDoc } from 'firebase/firestore';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import {
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  query,
+  setDoc,
+  where,
+} from 'firebase/firestore';
 import { db } from '../../firebase/clientApp';
 
 export const createGroup = createAsyncThunk(
   'groups/createGroup',
   async (groupData) => {
-    const id = groupData.id;
+    const id = groupData.path;
     await setDoc(doc(db, 'groups', id), groupData);
 
     return groupData;
@@ -24,29 +31,66 @@ export const setGroupPlayer = createAsyncThunk(
   }
 );
 
+export const getCurrentGroup = createAsyncThunk(
+  'groups/getCurrentGroup',
+  async (groupPath) => {
+    const docRef = doc(db, 'groups', groupPath);
+    const docSnap = await getDoc(docRef);
+    return docSnap.data();
+  }
+);
+
+export const getGroupPlayers = createAsyncThunk(
+  'groups/getGroupPlayers',
+  async (groupPath) => {
+    const groupPlayers = {
+      path: groupPath,
+      players: {
+        core: [],
+        reserve: [],
+        admin: [],
+      },
+    };
+    const groupsRef = collection(db, 'group_users');
+    const queryCore = query(
+      groupsRef,
+      where('groupPath', '==', groupPath),
+      where('userStatus', '==', 'core')
+    );
+    const querySnapshotCore = await getDocs(queryCore);
+    querySnapshotCore.forEach((doc) => {
+      const data = doc.data();
+      groupPlayers.players.core.push(data.userId);
+    });
+
+    const queryReserve = query(
+      groupsRef,
+      where('groupPath', '==', groupPath),
+      where('userStatus', '==', 'reserve')
+    );
+    const querySnapshotReserve = await getDocs(queryReserve);
+    querySnapshotReserve.forEach((doc) => {
+      const data = doc.data();
+      groupPlayers.players.reserve.push(data.userId);
+    });
+
+    const queryAdmin = query(
+      groupsRef,
+      where('groupPath', '==', groupPath),
+      where('userStatus', '==', 'admin')
+    );
+    const querySnapshotAdmin = await getDocs(queryAdmin);
+    querySnapshotAdmin.forEach((doc) => {
+      const data = doc.data();
+      groupPlayers.players.admin.push(data.userId);
+    });
+
+    return groupPlayers;
+  }
+);
+
 const initialState = {
-  byPath: {
-    // name: '',
-    // id: '',
-    // matches: [],
-    // players: {
-    //   core: [
-    //     'p2',
-    //     'p3',
-    //     'p4',
-    //     'p5',
-    //     'p6',
-    //     'p7',
-    //     'p8',
-    //     'p9',
-    //     'p10',
-    //     'p11',
-    //     'p12',
-    //   ],
-    //   reserve: ['p13', 'p14', 'p15', 'p16'],
-    //   admin: '',
-    // },
-  },
+  byPath: {},
   isLoading: false,
   failedToLoad: false,
   errorMessage: '',
@@ -95,6 +139,42 @@ export const groupsSlice = createSlice({
       state.isLoading = false;
       state.failedToLoad = true;
       state.errorMessage = action.payload;
+    });
+    builder.addCase(getCurrentGroup.fulfilled, (state, action) => {
+      state.byPath[action.payload.path] = {
+        ...action.payload,
+        matches: [],
+        players: {
+          core: [],
+          reserve: [],
+          admin: [],
+        },
+      };
+      state.failedToLoad = false;
+      state.isLoading = false;
+    });
+    builder.addCase(getCurrentGroup.pending, (state) => {
+      state.isLoading = true;
+      state.failedToLoad = false;
+    });
+    builder.addCase(getCurrentGroup.rejected, (state) => {
+      state.isLoading = false;
+      state.failedToLoad = true;
+    });
+    builder.addCase(getGroupPlayers.fulfilled, (state, action) => {
+      const groupPath = action.payload.path;
+      state.byPath[groupPath].players = action.payload.players;
+      state.isLoading = false;
+      state.failedToLoad = false;
+    });
+    builder.addCase(getGroupPlayers.pending, (state) => {
+      state.isLoading = true;
+      state.failedToLoad = false;
+    });
+    builder.addCase(getGroupPlayers.rejected, (state, action) => {
+      state.isLoading = false;
+      state.failedToLoad = true;
+      // state.errorMessage = action.payload;
     });
   },
 });
--- pages/dashboard/[groupDetail].js
+++ pages/dashboard/[groupDetail].js
@@ -1,10 +1,60 @@
-import GroupContainer from '../../components/groups/GroupContainer';
 import Layout from '../../components/layout/Layout';
+import { useRouter } from 'next/router';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  selectGroupsByPath,
+  selectIsLoading,
+  getCurrentGroup,
+  getGroupPlayers,
+} from '../../features/group/groupsSlice';
+import { selectCurrentUser } from '../../features/usersSlice';
+import MatchesListing from '../../components/match-listing/MatchesListing';
+import PlayerListing from '../../components/Players/PlayerListing';
+import { useEffect } from 'react';
 
 export default function GroupDetail() {
-  return (
-    <Layout>
-      <GroupContainer />
-    </Layout>
-  );
+  const router = useRouter();
+  const dispatch = useDispatch();
+  const currentPath = router.query.groupDetail;
+
+  dispatch(getCurrentGroup(currentPath));
+  dispatch(getGroupPlayers(currentPath));
+
+  // get current group
+  // get group matches
+  // get group players
+
+  // const isLoading = useSelector(selectIsLoading);
+  // const groupsByPath = useSelector(selectGroupsByPath);
+  // let currentGroup = groupsByPath[currentPath];
+
+  // const currentUser = useSelector(selectCurrentUser);
+  // const adminArr = currentGroup.players.admin;
+  // const isAdmin = adminArr.includes(currentUser.uid);
+
+  // const groupName = currentGroup.name;
+  // const players = currentGroup.players;
+  // const matches = currentGroup.matches;
+
+  // if (isLoading) {
+  //   return (
+  //     <Layout>
+  //       <h1>Loading</h1>
+  //     </Layout>
+  //   );
+  // }
+  return <p>h3ey</p>;
+  // <Layout>
+  //   <h2>{groupName}</h2>
+
+  //   {matches.length === 0 ? (
+  //     <p>No upcoming Matches</p>
+  //   ) : (
+  //     <MatchesListing type='upcomingMatches' display='1' matches={matches} />
+  //   )}
+  //   {isAdmin && <button>Create Match</button>}
+
+  //   {/* <PlayerListing type='group' players={players} /> */}
+  // </Layout>
+  // );
 }
