8a990928eade980da724a1a40ffdf73f0c0dd478
Cam Taliadoros <84043785+camtaliadoros@users.noreply.github.com>
2022-03-14 21:24:07 +0000

    share links

--- components/groups/GroupShare.js
+++ components/groups/GroupShare.js
@@ -1,15 +1,40 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faClone, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
+import { useRouter } from 'next/router';
+import { useState } from 'react';
 
 export default function GroupShare() {
+  const router = useRouter();
+  const currentRoute = router.asPath;
+  const [textCopied, setTextCopied] = useState(false);
+  // Add links
   return (
     <>
       <h3 className='title'>Invite New Players</h3>
       <div>
-        <FontAwesomeIcon icon={faWhatsapp} />
-        <FontAwesomeIcon icon={faEnvelope} />
-        <FontAwesomeIcon icon={faClone} />
+        <a
+          href={`https://wa.me/?text=Join%20my%20football%20group: https://matcher.com${currentRoute}`}
+          target='_blank'
+        >
+          <FontAwesomeIcon icon={faWhatsapp} />
+        </a>
+        <a
+          href={`mailto:?subject=Join my football group&body=Join my football group: https://matcher.com${currentRoute}`}
+        >
+          <FontAwesomeIcon icon={faEnvelope} />
+        </a>
+        <button
+          onClick={() => {
+            navigator.clipboard.writeText(
+              `Join my football group: https://matcher.com${currentRoute}`
+            );
+            setTextCopied(true);
+            setTimeout(() => setTextCopied(false), 3000);
+          }}
+        >
+          {textCopied ? <p>Copied</p> : <FontAwesomeIcon icon={faClone} />}
+        </button>
       </div>
     </>
   );
