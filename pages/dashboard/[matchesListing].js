4ce51121ca9129c15e28c0003eb37baa22a6cba2
Cam Taliadoros <84043785+camtaliadoros@users.noreply.github.com>
2022-09-21 19:15:18 +0100

    match listing long data

--- pages/dashboard/[matchesListing].js
+++ pages/dashboard/[matchesListing].js
@@ -1,21 +1,67 @@
 import { useRouter } from 'next/router';
-import { useSelector } from 'react-redux';
-import { selectMatchesPlaying } from '../../features/users/userSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  getUserMatches,
+  selectCurrentUserDetails,
+  selectMatchesInvited,
+  selectMatchesPendingPayment,
+  selectMatchesPlaying,
+  selectMatchesRequests,
+  selectUserMatches,
+} from '../../features/users/userSlice';
 import MatchesListing from '../../components/match-listing/MatchesListing';
 import Layout from '../../components/layout/Layout';
+import { useEffect } from 'react';
+import {
+  getMatchesById,
+  selectMatches,
+} from '../../features/matches/matchesSlice';
 
 export default function MatchesListingPage() {
   const router = useRouter();
+  const dispatch = useDispatch();
+  const user = useSelector(selectCurrentUserDetails);
   const listingType = router.query.matchesListing;
+  const allUserMatches = useSelector(selectUserMatches);
+  const matchesData = useSelector(selectMatches);
+
   const upcomingMatches = useSelector(selectMatchesPlaying);
+  const matchInvites = useSelector(selectMatchesInvited);
+  const matchesPendingPayment = useSelector(selectMatchesPendingPayment);
+  const matchRequests = useSelector(selectMatchesRequests);
+
+  useEffect(() => {
+    if (user.id) {
+      if (!allUserMatches.length) {
+        dispatch(getUserMatches(user.id));
+      }
+    }
+  }, [user.id]);
+
+  useEffect(() => {
+    let matchesToFetch;
+
+    if (listingType === 'upcoming-matches') {
+      matchesToFetch = upcomingMatches;
+    } else if (listingType === 'match-invites') {
+      matchesToFetch = matchInvites;
+    } else if (listingType === 'pending-payment') {
+      matchesToFetch = matchesPendingPayment;
+    } else if (listingType === 'match-requests') {
+      matchesToFetch = matchRequests;
+    }
+
+    matchesToFetch.length && dispatch(getMatchesById(matchesToFetch));
+  }, [
+    upcomingMatches.length,
+    matchInvites.length,
+    matchesPendingPayment.length,
+    matchRequests.length,
+  ]);
 
   return (
     <Layout>
-      <MatchesListing
-        display='all'
-        type={listingType}
-        matches={upcomingMatches}
-      />
+      <MatchesListing display='all' type={listingType} matches={matchesData} />
     </Layout>
   );
 }
