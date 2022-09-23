c85279ec91e9f13dddb1c029d88e7d0e0612cd9d
Cam Taliadoros <84043785+camtaliadoros@users.noreply.github.com>
2022-09-23 17:33:11 +0100

    nav bar desktop styling

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -1,17 +1,41 @@
+import {
+  faFutbol,
+  faUserGroup,
+  faUser,
+} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Link from 'next/link';
 import AuthNavigation from './AuthNavigation';
 
 export default function Header({ classes }) {
   return (
-    <header className={classes.header}>
-      <Link href='/'>
-        <a className='logo'>FA</a>
-      </Link>
-      <nav className='mobile-hidden'>
-        <AuthNavigation authClass={classes.navLinks} />
-        <Link href='/profile'>Profile</Link>
-        <Link href='/dashboard'>Dashboard</Link>
-      </nav>
-    </header>
+    <>
+      <header className={classes.header}>
+        <div>
+          <Link href='/'>
+            <a className='logo'>FA</a>
+          </Link>
+          <nav className='mobile-hidden'>
+            <AuthNavigation authClass={classes.navLinks} />
+          </nav>
+        </div>
+      </header>
+      <div className={classes.navBar}>
+        <div>
+          <Link href='/dashboard'>
+            <FontAwesomeIcon icon={faFutbol} />
+            Matches
+          </Link>
+        </div>
+        <div>
+          <FontAwesomeIcon icon={faUserGroup} />
+          <Link href='/dashboard'>Groups</Link>
+        </div>
+        <div>
+          <FontAwesomeIcon icon={faUser} />
+          <Link href='/profile'>Profile</Link>
+        </div>
+      </div>
+    </>
   );
 }
