f71a41d70d7c6c53df9a81f6bf97e6848b176a86
Cam Taliadoros <84043785+camtaliadoros@users.noreply.github.com>
2022-09-23 17:57:33 +0100

    header styling fix

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -11,14 +11,12 @@ export default function Header({ classes }) {
   return (
     <>
       <header className={classes.header}>
-        <div>
-          <Link href='/'>
-            <a className='logo'>FA</a>
-          </Link>
-          <nav className='mobile-hidden'>
-            <AuthNavigation authClass={classes.navLinks} />
-          </nav>
-        </div>
+        <Link href='/'>
+          <a className='logo'>FA</a>
+        </Link>
+        <nav className='mobile-hidden'>
+          <AuthNavigation authClass={classes.navLinks} />
+        </nav>
       </header>
       <div className={`${classes.navBar} mobile-hidden`}>
         <div>
