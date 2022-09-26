b767f72b745a5462024f748d3df9c637101f41ea
Cam Taliadoros <84043785+camtaliadoros@users.noreply.github.com>
2022-09-26 14:40:30 +0100

    adding link to group card

--- components/group-listing/GroupCard.js
+++ components/group-listing/GroupCard.js
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { selectGroups } from '../../features/groups/groupsSlice';
@@ -6,15 +7,19 @@ export default function GroupCard({ groupId }) {
   const groupsData = useSelector(selectGroups);
 
   const [groupName, setGroupName] = useState();
+  const [groupPath, setGroupPath] = useState();
 
   useEffect(() => {
     const currentGroup = groupsData.find((group) => group.id === groupId);
     setGroupName(currentGroup?.name);
+    setGroupPath(currentGroup?.path);
   }, [groupsData]);
 
   return (
-    <div className='card'>
-      <h4>{groupName}</h4>
-    </div>
+    <Link href={`../${groupPath}`}>
+      <div className='card'>
+        <h4>{groupName}</h4>
+      </div>
+    </Link>
   );
 }
